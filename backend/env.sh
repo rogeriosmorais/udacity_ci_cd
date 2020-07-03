c85229638d97bf1213e351e38c36dda3f63b187e
Rogerio Morais <rogeriosmorais@gmail.com>
2020-07-03 11:18:54 -0300

    Env variables

---
+++ backend/env.sh
@@ -0,0 +1,8 @@
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_HOST=udacitycicd.cvpi0ipgh545.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=rogerneo
+export TYPEORM_DATABASE=glee2
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_ENTITIES=./src/modules/**/*.entity.ts
