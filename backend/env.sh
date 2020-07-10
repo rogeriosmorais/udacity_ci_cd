dc5515557298d54a8e4274411758631f296ae472
Rogerio Morais <rogeriosmorais@gmail.com>
2020-07-10 11:26:36 -0300

    Changes

--- backend/env.sh
+++ backend/env.sh
@@ -3,6 +3,6 @@ export TYPEORM_HOST=udacity.cvpi0ipgh545.us-east-1.rds.amazonaws.com
 export TYPEORM_PORT=5432
 export TYPEORM_USERNAME=postgres
 export TYPEORM_PASSWORD=rogerneo
-export TYPEORM_DATABASE=glee2
+export TYPEORM_DATABASE=udacity
 export TYPEORM_MIGRATIONS=./src/migrations/*.ts
 export TYPEORM_ENTITIES=./src/modules/**/*.entity.ts
